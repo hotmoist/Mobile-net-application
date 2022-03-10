7e5c9e1a0370dc477135272ae8d56274f9a829e5
hotmosit <gye0203@gmail.com>
2022-03-10 21:35:30 +0900

    camerax initial setting

--- app/src/main/java/com/practice_app/image_detector/MainActivity.java
+++ app/src/main/java/com/practice_app/image_detector/MainActivity.java
@@ -1,14 +1,17 @@
 package com.practice_app.image_detector;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import com.google.common.util.concurrent.ListenableFuture;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
     }
 }
