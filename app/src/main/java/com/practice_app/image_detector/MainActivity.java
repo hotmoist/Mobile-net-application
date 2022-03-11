45b0258fd4130038fce76c14cc72766e5108c7ae
hotmosit <gye0203@gmail.com>
2022-03-11 21:35:50 +0900

    Camera permission request added

--- app/src/main/java/com/practice_app/image_detector/MainActivity.java
+++ app/src/main/java/com/practice_app/image_detector/MainActivity.java
@@ -2,16 +2,154 @@ package com.practice_app.image_detector;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import com.google.common.util.concurrent.ListenableFuture;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.ImageReader;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.practice_app.image_detector.env.Logger;
+
+public class MainActivity extends AppCompatActivity
+        implements ImageReader.OnImageAvailableListener,
+        Camera.PreviewCallback,
+        View.OnClickListener,
+        AdapterView.OnItemSelectedListener {
+    private static final Logger LOGGER = new Logger();
+
+    private static final int PERMISSIONS_REQUEST = 1;
+
+    private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
+    protected int previewWidth = 0;
+    protected int previewHeight = 0;
+    private Handler handler;
+    private HandlerThread handlerThread;
+    private boolean useCamera2API;
+
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(final Bundle savedInstanceState) {
+        LOGGER.d("onCreate" + this);
+        super.onCreate(null);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);   // keep the screen on
+
         setContentView(R.layout.activity_main);
-        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        if (hasPermission()) {
+            setFragment();
+        } else {
+            requestPermission();
+        }
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] bytes, Camera camera) {
+
+    }
+
+    @Override
+    public void onImageAvailable(ImageReader imageReader) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    // 장치의 하드웨어 레벨를 통해 적합 여부 파악
+    private boolean isHardwareLevelSupported(
+
+            CameraCharacteristics characteristics, int requiredLevel
+    ){
+        int deviceLevel = characteristics.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL);
+        if (deviceLevel == CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_LEGACY){
+            return requiredLevel == deviceLevel;
+        }
+
+        return requiredLevel <= deviceLevel;
+    }
+
+    private String chooseCamera() {
+        final CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            for (final String cameraId : manager.getCameraIdList()) {
+                final CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+                final Integer facing = characteristics.get(CameraCharacteristics.LENS_FACING);
+
+                // 해당 부분을 통해 후면, 전면 카메라 변경 가능
+                if (facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT) {
+                    continue;
+                }
+
+                final StreamConfigurationMap map =
+                        characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
+                if (map == null) {
+                    continue;
+                }
+
+                // Fallback to camera1 API for internal cameras that don't have full support.
+                // This should help with legacy situations where using the camera2 API causes
+                // distorted or otherwise broken previews.
+                useCamera2API =
+                        (facing == CameraCharacteristics.LENS_FACING_EXTERNAL) || isHardwareLevelSupported(
+                                characteristics, CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_FULL);
+                LOGGER.i("Camera API lv2?: %s", useCamera2API);
+                return cameraId;
+            }
+        } catch (CameraAccessException e) {
+            LOGGER.e(e, "카메라 접근이 불가능합니다");
+        }
+
+        return null;
+    }
+
+    protected void setFragment() {
+        String cameraID = chooseCamera();
+    }
+
+    private boolean hasPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkSelfPermission(PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return true;
+        }
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (shouldShowRequestPermissionRationale(PERMISSION_CAMERA)) {
+                Toast.makeText(
+                        MainActivity.this,
+                        "카메라 권한 설정이 필요합니다",
+                        Toast.LENGTH_LONG
+                ).show();
+            }
+            requestPermissions(new String[]{PERMISSION_CAMERA}, PERMISSIONS_REQUEST);
+        }
     }
 }
